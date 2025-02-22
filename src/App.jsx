0b2862232c04cccba0a2074dc6c54a6ab9b9aa06
Blessing Joshua <blessingjoshuaoffl@gmail.com>
2025-02-22 10:19:14 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='name'>Blessing Joshua</h1>
+      <h2 className='email'>blessingjoshua@gmail.com</h2>
+      <h2 className='phone'>Phone 08134567890</h2>
+      <h2 className='address'>Adress: No 1234567890</h2>
+
+    </div>
+  )
+}
+
+export default App
